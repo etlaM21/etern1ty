3963efc94faa358e58beb0005de4bfba3022a58b
Malte Hillebrand <maltehillebrand96@gmail.com>
2024-09-18 10:21:27 +0200

    switched to fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ int main( ){
 #endif
 
 	auto window = ofCreateWindow(settings);
-
+	ofSetFullscreen(true);
+	// ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
 	ofRunApp(window, make_shared<ofApp>());
 	ofRunMainLoop();
 
