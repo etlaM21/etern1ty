ad590d386fbb54ac9a4b086be1d1ba7de4a3e58e
Malte H <maltehillebrand96@gmail.com>
2026-01-29 13:53:50 +0100

    PI window fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,25 @@
 //========================================================================
 int main(int argc, char * argv[]) {
 
-#ifdef OF_TARGET_OPENGLES
-	ofGLESWindowSettings settings;
-	settings.glesVersion=2;
-#else
-	ofGLWindowSettings settings;
-	settings.setGLVersion(3,2);
-#endif
+	// Check if we are on a Raspberry Pi (Linux + ARM architecture)
+	#if defined(TARGET_RASPBERRY_PI) || defined(TARGET_LINUX_ARM)
+		ofGLESWindowSettings settings;
+		settings.glesVersion = 2;
+		settings.setSize(1280, 720);
+		settings.windowMode = OF_FULLSCREEN;
+	#else
+		ofGLWindowSettings settings;
+		settings.setGLVersion(3, 2);
+		settings.setSize(1280, 720);
+		settings.windowMode = OF_WINDOW; // Don't force fullscreen on your laptop
+	#endif
+
 	auto window = ofCreateWindow(settings);
 	auto app = make_shared<ofApp>();
 
 	ofSetFullscreen(false);
 	// ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
-	// Pass the arguments to a custom function in your ofApp
+	// Pass the arguments to a custom function in ofApp -> CMD LINE ARGUMENTS
 	app->arguments.assign(argv, argv + argc);
 
 	ofRunApp(window, app);
