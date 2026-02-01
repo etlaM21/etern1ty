d48252d945d39e8e8f491fb17c38bcdf459fa8f2
Malte H <maltehillebrand96@gmail.com>
2026-02-01 17:25:12 +0100

    1st working pi version, wrong colors

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,17 @@
 //========================================================================
 int main(int argc, char * argv[]) {
 
-	// Check if we are on a Raspberry Pi (Linux + ARM architecture)
-	#if defined(TARGET_RASPBERRY_PI) || defined(TARGET_LINUX_ARM)
-		ofGLESWindowSettings settings;
-		settings.glesVersion = 2;
-		settings.setSize(1280, 720);
-		settings.windowMode = OF_FULLSCREEN;
-	#else
-		ofGLWindowSettings settings;
-		settings.setGLVersion(3, 2);
-		settings.setSize(1280, 720);
-		settings.windowMode = OF_WINDOW; // Don't force fullscreen on your laptop
-	#endif
+	// ofGLESWindowSettings settings;
+	// settings.glesVersion = 2;
+	ofGLWindowSettings settings;
+	settings.setGLVersion(2, 1);
+	settings.setSize(1920, 1080);
+	settings.windowMode = OF_FULLSCREEN;
 
 	auto window = ofCreateWindow(settings);
 	auto app = make_shared<ofApp>();
 
-	ofSetFullscreen(false);
+	// ofSetFullscreen(false);
 	// ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
 	// Pass the arguments to a custom function in ofApp -> CMD LINE ARGUMENTS
 	app->arguments.assign(argv, argv + argc);
